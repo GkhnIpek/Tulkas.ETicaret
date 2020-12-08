9946f4381787056cfac82da1c0efb5d4aff2bead
GÖKHAN İPEK <gokipek91@gmail.com>
2020-12-08 21:10:04 +0300

    commit

---
+++ src/Tulkas.ETicaret/Tulkas.ETicaret.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tulkas.ETicaret.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+            
+        }
+    }
+}
---
+++ src/Tulkas.ETicaret/src/Tulkas.ETicaret.Infrastructure/DataContext/StoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tulkas.ETicaret.Core.DbModels;
+
+namespace Tulkas.ETicaret.Infrastructure.DataContext
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
